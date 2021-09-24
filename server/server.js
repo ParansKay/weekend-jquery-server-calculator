34514052243711fe91a2ed60c89dd85f97d8b1b9
Paran Kashani <parankashani@gmail.com>
2021-09-24 15:09:23 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+//requires
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// uses 
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//globals 
+
+const port = 5000;
+let calculatedNumbers = [];
+
+//spin up server
+
+app.listen(port, ()=>{
+    console.log( 'server up on:', port );
+})
+
+//routes 
